621fe4713f9dddae797fd80f1183be9032417604
NECSYDC\mag <marco.marigo@necsy.it>
2022-04-19 12:53:34 +0200

    Aggiunto un test di integrazione

---
+++ src/test/java/it/pagopa/pn/user/attributes/middleware/db/v1/ConsentDaoTestIT.java
@@ -0,0 +1,27 @@
+package it.pagopa.pn.user.attributes.middleware.db.v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ConsentDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void consentAction() {
+    }
+
+    @Test
+    void getConsentByType() {
+    }
+
+    @Test
+    void getConsents() {
+    }
+}
