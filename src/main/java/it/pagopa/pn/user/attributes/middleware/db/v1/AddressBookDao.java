b2bda60101a60ab1ef36557e011e6c31628bfceb
Rebecca Milione <rebecca.milione@cinetix.it>
2022-05-05 14:00:52 +0200

    errore commento

--- src/main/java/it/pagopa/pn/user/attributes/middleware/db/v1/AddressBookDao.java
+++ src/main/java/it/pagopa/pn/user/attributes/middleware/db/v1/AddressBookDao.java
@@ -139,7 +139,7 @@ public class AddressBookDao extends BaseDao {
     }
 
     /**
-     * Inserice o aggiorna un item di tipo AddressBookEntity e VerifiedAddress
+     * Inserisce o aggiorna un item di tipo AddressBookEntity e VerifiedAddress
      *
      * @param addressBook indirizzo da salvare
      * @param verifiedAddress verifiedaddress da salvare
