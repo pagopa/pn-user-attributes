62d580a8be7c8293893e88578997b36bac85113d
Rebecca Milione <rebecca.milione@cinetix.it>
2022-05-10 17:49:21 +0200

    test di consentDao

---
+++ src/test/java/it/pagopa/pn/user/attributes/middleware/db/AddressBookDaoTestIT.java
@@ -0,0 +1,36 @@
+package it.pagopa.pn.user.attributes.middleware.db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookDaoTest {
+
+    @Test
+    void deleteAddressBook() {
+    }
+
+    @Test
+    void getAddresses() {
+    }
+
+    @Test
+    void getAllAddressesByRecipient() {
+    }
+
+    @Test
+    void saveVerificationCode() {
+    }
+
+    @Test
+    void getVerificationCode() {
+    }
+
+    @Test
+    void validateHashedAddress() {
+    }
+
+    @Test
+    void saveAddressBookAndVerifiedAddress() {
+    }
+}
