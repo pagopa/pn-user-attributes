c410a253a4e613668addcfa7c11d866aa182108d
Rebecca Milione <rebecca.milione@cinetix.it>
2022-05-11 16:11:49 +0200

    test di deleteAddressBook

--- src/test/java/it/pagopa/pn/user/attributes/middleware/db/AddressBookDaoTestIT.java
+++ src/test/java/it/pagopa/pn/user/attributes/middleware/db/AddressBookDaoTestIT.java
@@ -1,10 +1,24 @@
 package it.pagopa.pn.user.attributes.middleware.db;
 
+import it.pagopa.pn.user.attributes.config.PnUserattributesConfig;
+import it.pagopa.pn.user.attributes.middleware.db.entities.AddressBookEntity;
+import it.pagopa.pn.user.attributes.middleware.db.entities.ConsentEntity;
+import it.pagopa.pn.user.attributes.middleware.db.entities.VerifiedAddressEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @ExtendWith(SpringExtension.class)
 @TestPropertySource(properties = {
@@ -14,33 +28,80 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 })
 @SpringBootTest
 class AddressBookDaoTestIT {
+    private final Duration d = Duration.ofMillis(3000);
+
+    @Autowired
+    private AddressBookDao addressBookDao;
+
+    @Autowired
+    DynamoDbEnhancedAsyncClient dynamoDbEnhancedAsyncClient;
+
+    @Autowired
+    PnUserattributesConfig pnUserattributesConfig;
+
+    TestDao<AddressBookEntity> testDao;
+
+    @BeforeEach
+    void setup() {
+        testDao = new TestDao(dynamoDbEnhancedAsyncClient, pnUserattributesConfig.getDynamodbTableName(), AddressBookEntity.class);
+    }
 
     @Test
     void deleteAddressBook() {
 
-    }
+        //Given
+        AddressBookEntity addressBookToDelete = newAddress();
+        VerifiedAddressEntity verifiedAddress = new VerifiedAddressEntity("VA-123e4567-e89b-12d3-a456-426614174000", "Legal", "SMS");
+        addressBookDao.saveAddressBookAndVerifiedAddress(addressBookToDelete, verifiedAddress).block(d);
 
-    @Test
-    void getAddresses() {
-    }
+        //When
+        addressBookDao.deleteAddressBook(addressBookToDelete.getRecipientId(), addressBookToDelete.getSenderId(), addressBookToDelete.getAddressType(), addressBookToDelete.getChannelType());
 
-    @Test
-    void getAllAddressesByRecipient() {
-    }
+        //Then
+        try {
+            AddressBookEntity elementFromDb = testDao.get(addressBookToDelete.getPk(), addressBookToDelete.getSk());
+            Assertions.assertNull(elementFromDb);
+        } catch (Exception e) {
+            fail(e);
+        } finally {
+            try {
+                testDao.delete(addressBookToDelete.getPk(), addressBookToDelete.getSk());
+            } catch (Exception e) {
+                System.out.println("Nothing to remove");
+            }
 
-    @Test
-    void saveVerificationCode() {
+        }
     }
+        @Test
+        void getAddresses () {
 
-    @Test
-    void getVerificationCode() {
-    }
 
-    @Test
-    void validateHashedAddress() {
-    }
+        }
 
-    @Test
-    void saveAddressBookAndVerifiedAddress() {
+        @Test
+        void getAllAddressesByRecipient () {
+        }
+
+        @Test
+        void saveVerificationCode () {
+        }
+
+        @Test
+        void getVerificationCode () {
+        }
+
+        @Test
+        void validateHashedAddress () {
+        }
+
+        @Test
+        void saveAddressBookAndVerifiedAddress () {
+        }
+
+        public static AddressBookEntity newAddress() {
+            AddressBookEntity ab = new AddressBookEntity("AB-123e4567-e89b-12d3-a456-426614174000", "LEGAL", "default", "SMS");
+            ab.setCreated(Instant.now());
+            ab.setLastModified(Instant.now());
+            return ab;
+        }
     }
-}
