a4f985bf851c0043ab1976438f0042fab8704904
Rebecca Milione <rebecca.milione@cinetix.it>
2022-05-11 17:36:51 +0200

    test di validateHashAdd, getaddresss

--- src/test/java/it/pagopa/pn/user/attributes/middleware/db/AddressBookDaoTestIT.java
+++ src/test/java/it/pagopa/pn/user/attributes/middleware/db/AddressBookDaoTestIT.java
@@ -16,6 +16,7 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -52,7 +53,12 @@ class AddressBookDaoTestIT {
         //Given
         AddressBookEntity addressBookToDelete = newAddress();
         VerifiedAddressEntity verifiedAddress = new VerifiedAddressEntity("VA-123e4567-e89b-12d3-a456-426614174000", "Legal", "SMS");
-        addressBookDao.saveAddressBookAndVerifiedAddress(addressBookToDelete, verifiedAddress).block(d);
+        try {
+            testDao.delete(addressBookToDelete.getPk(), addressBookToDelete.getSk());
+            addressBookDao.saveAddressBookAndVerifiedAddress(addressBookToDelete,verifiedAddress).block(d);
+        } catch (Exception e) {
+            System.out.println("error removing");
+        }
 
         //When
         addressBookDao.deleteAddressBook(addressBookToDelete.getRecipientId(), addressBookToDelete.getSenderId(), addressBookToDelete.getAddressType(), addressBookToDelete.getChannelType());
@@ -75,8 +81,49 @@ class AddressBookDaoTestIT {
         @Test
         void getAddresses () {
 
+                //Given
+                VerifiedAddressEntity verifiedAddress =new VerifiedAddressEntity("VA-123e4567-e89b-12d3-a456-426614174000","Legal","SMS");
+                List<AddressBookEntity> toInsert = new ArrayList<>();
+                AddressBookEntity addressToInsert = newAddress();
+                addressToInsert.setSk("Legal");
+                toInsert.add(addressToInsert);
+                addressToInsert = newAddress();
+                addressToInsert.setSk("courtesy");
+                toInsert.add(addressToInsert);
 
-        }
+                try {
+                    toInsert.forEach(x -> {
+                        try {
+                            testDao.delete(x.getPk(), x.getSk());
+                            addressBookDao.saveAddressBookAndVerifiedAddress(x,verifiedAddress ).block(d);
+                        } catch (Exception e) {
+                            System.out.println("error removing");
+                        }
+                    });
+                } catch (Exception e) {
+                    System.out.println("Nothing to remove");
+                }
+
+                //WHEN
+            List<AddressBookEntity> results = addressBookDao.getAddresses(addressToInsert.getRecipientId(),addressToInsert.getSenderId(), addressToInsert.getAddressType()).collectList().block(d);
+
+                //THEN
+                try {
+                    Assertions.assertNotNull(results);
+                    Assertions.assertEquals(2, results.size());
+                    Assertions.assertTrue(toInsert.contains(results.get(0)));
+                    Assertions.assertTrue(toInsert.contains(results.get(1)));
+                } catch (Exception e) {
+                    throw new RuntimeException();
+                } finally {
+                    try {
+                        testDao.delete(addressToInsert.getPk(), addressToInsert.getSk());
+                    } catch (Exception e) {
+                        System.out.println("Nothing to remove");
+                    }
+                }
+                }
+
 
         @Test
         void getAllAddressesByRecipient () {
@@ -90,9 +137,34 @@ class AddressBookDaoTestIT {
         void getVerificationCode () {
         }
 
-        @Test
-        void validateHashedAddress () {
+    void validateHashedAddress() {
+
+        //Given
+        AddressBookEntity addressBook = newAddress();
+        VerifiedAddressEntity verifiedAddress = new VerifiedAddressEntity("VA-123e4567-e89b-12d3-a456-426614174000", "Legal", "SMS");
+        try {
+            testDao.delete(addressBook.getPk(), addressBook.getSk());
+            addressBookDao.saveAddressBookAndVerifiedAddress(addressBook, verifiedAddress).block(d);
+        } catch (Exception e) {
+            System.out.println("error removing");
         }
+
+        //When
+        AddressBookDao.CHECK_RESULT result = addressBookDao.validateHashedAddress(addressBook.getRecipientId(), "hashAddressgahs67323525").block(d);
+
+        //Then
+        try {
+            Assertions.assertEquals("CHECK_RESULT.NOT_EXISTS", result);
+        } catch (Exception e) {
+            fail(e);
+        } finally {
+            try {
+                testDao.delete(addressBook.getPk(), addressBook.getSk());
+            } catch (Exception e) {
+                System.out.println("Nothing to remove");
+            }
+        }
+    }
 
         @Test
         void saveAddressBookAndVerifiedAddress () {
