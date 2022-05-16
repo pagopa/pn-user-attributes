5a791a0ee832936ed3b2397febe024a117a0a805
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-05-16 15:57:53 +0200

    test coverage

--- src/test/java/it/pagopa/pn/user/attributes/middleware/wsclient/PnExternalChannelClientTest.java
+++ src/test/java/it/pagopa/pn/user/attributes/middleware/wsclient/PnExternalChannelClientTest.java
@@ -2,18 +2,24 @@ package it.pagopa.pn.user.attributes.middleware.wsclient;
 
 import it.pagopa.pn.user.attributes.generated.openapi.server.rest.api.v1.dto.CourtesyChannelTypeDto;
 import it.pagopa.pn.user.attributes.generated.openapi.server.rest.api.v1.dto.LegalChannelTypeDto;
+import it.pagopa.pn.user.attributes.user.attributes.microservice.msclient.generated.datavault.v1.dto.BaseRecipientDtoDto;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.model.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
@@ -30,6 +36,9 @@ class PnExternalChannelClientTest {
 
     @Autowired
     private PnExternalChannelClient pnExternalChannelClient;
+
+    @MockBean
+    PnDataVaultClient pnDataVaultClient;
 
     private static ClientAndServer mockServer;
     @BeforeAll
@@ -52,6 +61,14 @@ class PnExternalChannelClientTest {
         CourtesyChannelTypeDto courtesyChannelType = null;
         String verificationCode = "12345";
         String path = "/external-channels/v1/digital-deliveries/legal-full-message-requests/.*";
+
+        BaseRecipientDtoDto baseRecipientDtoDto = new BaseRecipientDtoDto();
+        baseRecipientDtoDto.setInternalId(recipientId);
+        baseRecipientDtoDto.setDenomination("mario rossi");
+        List<BaseRecipientDtoDto> list = new ArrayList<>();
+        list.add(baseRecipientDtoDto);
+
+        Mockito.when(pnDataVaultClient.getRecipientDenominationByInternalId(Mockito.any())).thenReturn(Flux.fromIterable(list));
 
 
         new MockServerClient("localhost", 9998)
@@ -76,7 +93,13 @@ class PnExternalChannelClientTest {
         CourtesyChannelTypeDto courtesyChannelType = CourtesyChannelTypeDto.EMAIL;
         String verificationCode = "12345";
         String path = "/external-channels/v1/digital-deliveries/courtesy-full-message-requests/.*";
+        BaseRecipientDtoDto baseRecipientDtoDto = new BaseRecipientDtoDto();
+        baseRecipientDtoDto.setInternalId(recipientId);
+        baseRecipientDtoDto.setDenomination("mario rossi");
+        List<BaseRecipientDtoDto> list = new ArrayList<>();
+        list.add(baseRecipientDtoDto);
 
+        Mockito.when(pnDataVaultClient.getRecipientDenominationByInternalId(Mockito.any())).thenReturn(Flux.fromIterable(list));
 
         new MockServerClient("localhost", 9998)
                 .when(request()
