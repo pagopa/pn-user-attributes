883bf9b492657b6c3decf54e95c02758d6ff0c59
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-06-20 17:34:00 +0200

    introduzione dipendenza pn-common, pulizia codice

--- src/main/java/it/pagopa/pn/user/attributes/PnUserAttributesApplication.java
+++ src/main/java/it/pagopa/pn/user/attributes/PnUserAttributesApplication.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class PnUserAttributesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PnUserAttributesApplication.class, args);
+	}
+
+	public PnUserAttributesApplication(){
+
+		BlockHound.install();
 	}
 
 	@RestController
--- src/main/java/it/pagopa/pn/user/attributes/middleware/wsclient/common/BaseClient.java
+++ src/main/java/it/pagopa/pn/user/attributes/middleware/wsclient/common/BaseClient.java
@@ -2,13 +2,14 @@ package it.pagopa.pn.user.attributes.middleware.wsclient.common;
 
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
+import it.pagopa.pn.commons.pnclients.CommonBaseClient;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
 import java.util.concurrent.TimeUnit;
 
-public abstract class BaseClient {
+public abstract class BaseClient extends CommonBaseClient {
 
     protected BaseClient( ){
     }
@@ -18,7 +19,7 @@ public abstract class BaseClient {
         HttpClient httpClient = HttpClient.create().option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
                 .doOnConnected(connection -> connection.addHandlerLast(new ReadTimeoutHandler(10000, TimeUnit.MILLISECONDS)));
 
-        return builder.clientConnector(new ReactorClientHttpConnector(httpClient))
+        return super.enrichBuilder(builder.clientConnector(new ReactorClientHttpConnector(httpClient)))
                 .build();
     }
 }
