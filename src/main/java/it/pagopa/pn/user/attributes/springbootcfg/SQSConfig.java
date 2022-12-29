1b98ea5147f403f79876e96ddd63ea3246811801
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2022-12-29 16:26:25 +0100

    PN-2896 gestione log di errore su sqs

---
+++ src/main/java/it/pagopa/pn/user/attributes/springbootcfg/SQSConfig.java
@@ -0,0 +1,58 @@
+package it.pagopa.pn.user.attributes.springbootcfg;
+
+
+import com.amazonaws.Request;
+import com.amazonaws.Response;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.handlers.RequestHandler2;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import it.pagopa.pn.commons.configs.aws.AwsConfigs;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+@Configuration
+public class SQSConfig {
+
+    private final AwsConfigs awsConfigs;
+
+    public SQSConfig(AwsConfigs awsConfigs) {
+        this.awsConfigs = awsConfigs;
+    }
+
+    @Bean
+    public AmazonSQSAsync amazonSQS() {
+        if (StringUtils.hasText(awsConfigs.getEndpointUrl()))
+            return AmazonSQSAsyncClientBuilder.standard()
+                    .withRequestHandlers(new CustomAWSRequestHandler())
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsConfigs.getEndpointUrl(), awsConfigs.getRegionCode()))
+                    .build();
+        else
+            return AmazonSQSAsyncClientBuilder.standard()
+                    .withRegion(awsConfigs.getRegionCode())
+                    .build();
+    }
+
+    /**
+     * Questa Handler cattura tutte le richieste, risposte ed errori di oggetti di AWS come {@link AmazonSQSAsync}.
+     * È possibile catturare gli eventi sovrascrivendo i metodi di {@link RequestHandler2}.
+     * <p>
+     * È possibile associare l'Handler durante la creazione dell'oggetto AWS, ad esempio:
+     * <p>
+     * AmazonSQSAsyncClientBuilder.standard()
+     *             .withRequestHandlers(new CustomAWSRequestHandler())
+     *             .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsConfigs.getEndpointUrl(), awsConfigs.getRegionCode()))
+     *             .build();
+     *
+     */
+    @Slf4j
+    static class CustomAWSRequestHandler extends RequestHandler2 {
+
+        @Override
+        public void afterError(Request<?> request, Response<?> response, Exception e) {
+            log.error("Exception for: " + request.getOriginalRequestObject(), e);
+        }
+    }
+}
