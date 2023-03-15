0d7af9cb2a5f5adc761e9abfaa3eeacaa6c32729
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-03-15 17:33:33 +0100

    PN-4492: codegen update

--- scripts/openapi/internal-to-external.sh
+++
@@ -1,9 +0,0 @@
-cat docs/openapi/pn-address-book-api-internal-v1.yaml \
-    | grep -v "# NO EXTERNAL" \
-    | sed -e '/# ONLY EXTERNAL/s/^#//' \
-    > docs/openapi/pn-address-book-api-external-v1.yaml
-
-cat docs/openapi/pn-user-consents-api-internal-v1.yaml \
-    | grep -v "# NO EXTERNAL" \
-    | sed -e '/# ONLY EXTERNAL/s/^#//' \
-    > docs/openapi/pn-user-consents-api-external-v1.yaml
