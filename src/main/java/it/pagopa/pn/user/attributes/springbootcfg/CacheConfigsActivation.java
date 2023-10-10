bcb41a0d4e8596626fd22090ae9826a2dd3bdf1b
Ivan Rosina <ivan.rosina@cinetix-team.it>
2023-10-10 16:34:51 +0200

    nuova versione cache

--- src/main/java/it/pagopa/pn/user/attributes/springbootcfg/CacheConfigsActivation.java
+++ src/main/java/it/pagopa/pn/user/attributes/springbootcfg/CacheConfigsActivation.java
@@ -1,11 +1,10 @@
 package it.pagopa.pn.user.attributes.springbootcfg;
 
-import it.pagopa.pn.commons.configs.cache.CacheConfigs;
 import it.pagopa.pn.commons.configs.cache.PnCacheConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ PnCacheConfiguration.class})
-public class CacheConfigsActivation extends CacheConfigs {
+public class CacheConfigsActivation extends PnCacheConfiguration
+{
+
 }
