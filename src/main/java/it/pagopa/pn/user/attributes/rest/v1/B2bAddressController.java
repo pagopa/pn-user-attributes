09b4950db8b647341a58e96c54adfe4ea3302cd3
federica.moschese <federica.moschese@emeal.nttdata.com>
2024-08-07 14:58:48 +0200

    PN-12308 - created a new controller to check courtesy address

---
+++ src/main/java/it/pagopa/pn/user/attributes/rest/v1/B2bAddressController.java
@@ -0,0 +1,31 @@
+package it.pagopa.pn.user.attributes.rest.v1;
+
+import it.pagopa.pn.user.attributes.services.AddressBookService;
+import it.pagopa.pn.user.attributes.user.attributes.b2b.generated.openapi.server.v1.api.CourtesyApi;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.server.v1.dto.CourtesyDigitalAddressDto;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.server.v1.dto.CxTypeAuthFleetDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class B2bAddressController implements CourtesyApi {
+    private final AddressBookService addressBookService;
+
+    public B2bAddressController(AddressBookService addressBookService) {
+        this.addressBookService = addressBookService;
+    }
+
+    @Override
+    public Mono<ResponseEntity<Flux<CourtesyDigitalAddressDto>>> getCourtesyAddressBySender(String xPagopaPnCxId, CxTypeAuthFleetDto xPagopaPnCxType, String senderId, List<String> xPagopaPnCxGroups, String xPagopaPnCxRole, final ServerWebExchange exchange) {
+        log.info("getCourtesyAddressBySender - recipientId={} - senderId={}", xPagopaPnCxId, senderId);
+        return Mono.fromSupplier(() -> ResponseEntity.ok(addressBookService.getCourtesyAddressByRecipientAndSender(xPagopaPnCxId, senderId)));
+    }
+
+}
