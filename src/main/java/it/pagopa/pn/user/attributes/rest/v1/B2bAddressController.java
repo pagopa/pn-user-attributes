fdd3d84dd06372ef9dd46e8896efe86be4a47f7c
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-08-20 17:38:42 +0200

    [PN-12307] Fix B2bAddressController import

--- src/main/java/it/pagopa/pn/user/attributes/rest/v1/B2bAddressController.java
+++ src/main/java/it/pagopa/pn/user/attributes/rest/v1/B2bAddressController.java
@@ -1,9 +1,9 @@
 package it.pagopa.pn.user.attributes.rest.v1;
 
 import it.pagopa.pn.user.attributes.services.AddressBookService;
-import it.pagopa.pn.user.attributes.user.attributes.b2b.generated.openapi.server.v1.api.LegalApi;
-import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.server.v1.dto.CxTypeAuthFleetDto;
+import it.pagopa.pn.user.attributes.user.attributes.b2b.generated.openapi.server.v1.api.LegalB2BApi;
 import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.server.v1.dto.LegalDigitalAddressDto;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.server.v1.dto.CxTypeAuthFleetDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @Slf4j
-public class B2bAddressController implements LegalApi {
+public class B2bAddressController implements LegalB2BApi {
     private final AddressBookService addressBookService;
 
     public B2bAddressController(AddressBookService addressBookService) {
@@ -23,7 +23,7 @@ public class B2bAddressController implements LegalApi {
     }
 
     @Override
-    public Mono<ResponseEntity<Flux<LegalDigitalAddressDto>>> getLegalAddressBySender(String xPagopaPnCxId, CxTypeAuthFleetDto xPagopaPnCxType, String senderId, List<String> xPagopaPnCxGroups, String xPagopaPnCxRole, final ServerWebExchange exchange) {
+    public Mono<ResponseEntity<Flux<LegalDigitalAddressDto>>> getLegalAddressBySenderB2B(String xPagopaPnCxId, CxTypeAuthFleetDto xPagopaPnCxType, String senderId, List<String> xPagopaPnCxGroups, String xPagopaPnCxRole, final ServerWebExchange exchange) {
         log.info("getLegalAddressBySender - recipientId={} - senderId={}", xPagopaPnCxId, senderId);
         return Mono.fromSupplier(() -> ResponseEntity.ok(addressBookService.getLegalAddressByRecipientAndSender(xPagopaPnCxId, senderId)));
     }
