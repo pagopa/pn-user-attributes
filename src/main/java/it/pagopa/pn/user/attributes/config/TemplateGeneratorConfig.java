5b449729e33b6af1f86c05858b0c176bcbc5197b
adrian.borta <adrian.borta@accenture.com>
2024-12-09 17:58:09 +0100

    feat[PN-13362]: add TemplateGeneratorByDocComposition implements TemplateGenerator

---
+++ src/main/java/it/pagopa/pn/user/attributes/config/TemplateGeneratorConfig.java
@@ -0,0 +1,34 @@
+package it.pagopa.pn.user.attributes.config;
+
+import it.pagopa.pn.user.attributes.middleware.templates.TemplateGenerator;
+import it.pagopa.pn.user.attributes.middleware.templates.impl.TemplateGeneratorByClient;
+import it.pagopa.pn.user.attributes.middleware.templates.impl.TemplateGeneratorByDocComposition;
+import it.pagopa.pn.user.attributes.middleware.wsclient.templatesengine.impl.TemplatesClientImpl;
+import it.pagopa.pn.user.attributes.utils.DocumentComposition;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class TemplateGeneratorConfig {
+
+    private final DocumentComposition documentComposition;
+    private final TemplatesClientImpl templatesClient;
+
+    private final PnUserattributesConfig pnUserattributesConfig;
+
+    @Bean
+    @ConditionalOnProperty(name = "pn.user-attributes.enableTemplatesEngine", havingValue = "true", matchIfMissing = true)
+    public TemplateGenerator templateGeneratorClient() {
+        return new TemplateGeneratorByClient(templatesClient);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "pn.user-attributes.enableTemplatesEngine", havingValue = "false")
+    public TemplateGenerator templateGeneratorDocComposition() {
+        return new TemplateGeneratorByDocComposition(documentComposition, pnUserattributesConfig);
+    }
+
+}
---
+++ src/main/java/it/pagopa/pn/user/attributes/middleware/templates/impl/TemplateGeneratorByClient.java
@@ -0,0 +1,59 @@
+package it.pagopa.pn.user.attributes.middleware.templates.impl;
+
+import it.pagopa.pn.user.attributes.middleware.templates.TemplateGenerator;
+import it.pagopa.pn.user.attributes.middleware.wsclient.templatesengine.impl.TemplatesClientImpl;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.msclient.templatesengine.model.LanguageEnum;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.msclient.templatesengine.model.MailVerificationCodeBody;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.msclient.templatesengine.model.PecVerificationCodeBody;
+
+public record TemplateGeneratorByClient(TemplatesClientImpl templatesClient) implements TemplateGenerator {
+
+    @Override
+    public String generateEmailBody(String verificationCode) {
+        MailVerificationCodeBody mailVerificationCodeBody = new MailVerificationCodeBody();
+        mailVerificationCodeBody.setVerificationCode(verificationCode);
+        return templatesClient.mailVerificationCodeBody(LanguageEnum.IT, mailVerificationCodeBody);
+    }
+
+    @Override
+    public String generateEmailSubject() {
+        return templatesClient.mailVerificationCodeSubject(LanguageEnum.IT);
+    }
+
+    @Override
+    public String generatePecBody(String verificationCode) {
+        PecVerificationCodeBody pecVerificationCodeBody = new PecVerificationCodeBody();
+        pecVerificationCodeBody.setVerificationCode(verificationCode);
+        return templatesClient.pecVerificationCodeBody(LanguageEnum.IT, pecVerificationCodeBody);
+    }
+
+    @Override
+    public String generatePecConfirmBody() {
+        return templatesClient.pecValidationContactsSuccessBody(LanguageEnum.IT);
+    }
+
+    @Override
+    public String generatePecRejectBody() {
+        return templatesClient.pecValidationContactsRejectBody(LanguageEnum.IT);
+    }
+
+    @Override
+    public String generatePecSubject() {
+        return templatesClient.pecVerificationCodeSubject(LanguageEnum.IT);
+    }
+
+    @Override
+    public String generatePecSubjectConfirm() {
+        return templatesClient.pecValidationContactsSuccessSubject(LanguageEnum.IT);
+    }
+
+    @Override
+    public String generatePecSubjectReject() {
+        return templatesClient.pecValidationContactsRejectSubject(LanguageEnum.IT);
+    }
+
+    @Override
+    public String generateSmsBody() {
+        return templatesClient.smsVerificationCodeBody(LanguageEnum.IT);
+    }
+}
