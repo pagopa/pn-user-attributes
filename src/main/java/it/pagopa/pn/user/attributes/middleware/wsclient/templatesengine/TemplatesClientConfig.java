70e9c15dcf4c7b73e45a5d186fd4fba0c0957589
adrian.borta <adrian.borta@accenture.com>
2024-12-09 14:08:05 +0100

    [feat]-[PN-13362]: add TemplatesClientConfig

---
+++ src/main/java/it/pagopa/pn/user/attributes/middleware/wsclient/templatesengine/TemplatesClientConfig.java
@@ -0,0 +1,24 @@
+package it.pagopa.pn.user.attributes.middleware.wsclient.templatesengine;
+
+import it.pagopa.pn.user.attributes.config.PnUserattributesConfig;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.msclient.templatesengine.ApiClient;
+import it.pagopa.pn.user.attributes.user.attributes.generated.openapi.msclient.templatesengine.api.TemplateApi;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class TemplatesClientConfig {
+
+    @Bean
+    @Primary
+    public TemplateApi templateApiConfig(@Qualifier("withTracing") RestTemplate restTemplate,
+                                         PnUserattributesConfig cfg) {
+        ApiClient apiClient = new ApiClient(restTemplate);
+        apiClient.setBasePath(cfg.getClientDatavaultBasepath());
+        return new TemplateApi(apiClient);
+    }
+
+}
