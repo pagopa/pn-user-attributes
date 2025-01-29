4cdc5c8b40f8276edf0344d7e49337a7fe9ef1cf
michelescara <79908782+michelescara@users.noreply.github.com>
2025-01-29 16:34:36 +0100

    Hotfix/pn 13733 (#240)
    * PN-13733: Fixed handling of PEC address in pecValidationExpired lambda.
    Added unit tests.
    Added environment variables in microservice.yml
    
    * PN-13733: Added VPC configuration for pecValidationExpired lambda.
    
    * PN-13733: Updated package-lock.json
    
    * PN-13733: Fake commit to rebuild
    
    * PN-13733: Fixed SubnetIds definition in PecValidationExpiredLambda configuration.
    
    * PN-13733: Fixed env variables usage for http request.
    
    * PN-13733: Fixed hostname property.
    
    * PN-13733: Fixed datavault client call.
    Fixed error handling.
    Fixed address valorization.

--- functions/pecValidationExpired/src/app/lib/sqs.js
+++ functions/pecValidationExpired/src/app/lib/sqs.js
@@ -1,5 +1,5 @@
 const { SQSClient, SendMessageBatchCommand } = require("@aws-sdk/client-sqs");
-
+const { getVerificationCodeAddressByInternalId } = require("./datavaultClient.js")
 const sqs = new SQSClient({ region: process.env.REGION });
 const QUEUE_URL = process.env.QUEUE_URL
 
@@ -14,19 +14,33 @@ exports.sendMessages = async function sendMessages(messages) {
       }
 
       // i messaggi contengono info sensibili, non posso stampare l'input liscio
-      input.Entries.forEach((i) => {
+    await Promise.all(input.Entries.map(async (i) => {
           let body = JSON.parse(i.MessageBody);
 
-          const em = body.address.indexOf('@');
+          // Recuperiamo l'indirizzo da datavault, in quanto non viene piu' valorizzato nel record VC su pn-UserAttributes.
+          // Se non dovesse essere presente su datavault, è ancora presente nella tabella e lo recuperiamo da lì.
+          let address = await getVerificationCodeAddressByInternalId(body.internalId, body.hashedAddress);
+          address = address == null ? body.address : address;
+
+          if (address == null) {
+            throw new Error(`Address is null for recipientId "${body.internalId}" and hashedAddress "${body.hashedAddress}"`);
+          }
+
+          // Sovrascriviamo il campo address del MessageBody originale.
+          body.address = address;
+          i.MessageBody = JSON.stringify(body);
+
+          // Anonimizziamo l'indirizzo per poterlo stampare nel log
+          const em = address.indexOf('@');
           const startIndex = em * .2 | 0;
           const endIndex   = em * .9 | 0;
-          const anonymEm = body.address.slice(0, startIndex) +
-                 body.address.slice(startIndex, endIndex).replace(/./g, '*') +
-                 body.address.slice(endIndex);
+          const anonymEm = address.slice(0, startIndex) +
+                 address.slice(startIndex, endIndex).replace(/./g, '*') +
+                 address.slice(endIndex);
           body.address = anonymEm;
 
           console.log("sending message id:" +  i.Id + " eventId:" + i.MessageAttributes.eventId.StringValue + " message:" + JSON.stringify(body));
-        })
+        }));
 
       const command = new SendMessageBatchCommand(input);
       const response = await sqs.send(command);
